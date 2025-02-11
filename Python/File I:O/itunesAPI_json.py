0fc4b9294f4e6e93a186eaaad22fe6e9fb416eb8
Swaroop Mula <swaroopmulawork@gmail.com>
2025-02-10 20:21:39 -0600

    changed itunes code

--- Python/File I:O/itunesAPI_json.py
+++ Python/File I:O/itunesAPI_json.py
@@ -5,14 +5,12 @@ import json
 artist_name = input("Enter an Artist Name: ")
 n = int(input("No of songs: "))
 
-response = requests.get(f"https://itunes.apple.com/search?entity=song&limit=2&term=" + artist_name)
+response = requests.get(f"https://itunes.apple.com/search?entity=song&limit={n}&term=" + artist_name)
 
 deets = response.json()
-#use "json.dumps(deets, indent = 2)" for clean representation of entire json file
+# print(json.dumps(deets, indent = 2)) 
 
-print(json.dumps(deets, indent = 2))
-
-# print()
-# for item in deets.get("results"):
-#     print(item.get("trackName"))
-# print()
+print()
+for item in deets.get("results")[:n]:
+    print(item.get("trackName"))
+print()
