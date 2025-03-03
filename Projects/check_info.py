2b96a9eb187fab7b6b206f79a9eeb313497ed68e
Swaroop Mula <swaroopmulawork@gmail.com>
2025-03-03 17:26:20 -0500

    Changed folder

---
+++ Projects/check_info.py
@@ -0,0 +1,10 @@
+import pandas
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from helper import get_df
+
+file_path = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
+
+df = get_df(file_path)
+df.info(verbose=True)
