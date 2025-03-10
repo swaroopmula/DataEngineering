aa1f9b69eb34c736220d2d923ea5a65de0571ea5
Swaroop Mula <swaroopmulawork@gmail.com>
2025-03-10 16:13:18 -0500

    more info

--- Projects/helper/check_info.py
+++ Projects/helper/check_info.py
@@ -3,7 +3,26 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from helper.extract import get_df
 
-file_path = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
+def check_info(file_path):
 
-df = get_df(file_path)
-df.info(verbose=True)
+    df = get_df(file_path)
+    
+    print("\nDataFrame Info:")
+    print("----------------------------\n")
+    df.info(verbose=True)
+    
+    print("\n\nFirst 5 Rows:")
+    print("----------------------------\n")
+    print(df.head())
+    
+    print("\n\nMissing Values per Column:")
+    print("----------------------------\n")
+    print(df.isnull().sum())
+    
+    print("\n\nUnique Values per Column:")
+    print("----------------------------\n")
+    print(df.nunique())
+
+    print("\n")
+
+    return df
